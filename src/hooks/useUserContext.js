0bb3d847af852e5e3ecaf29c027f21005f1cb596
Aqief Syahmi <aqiefsyahmi@gmail.com>
2023-12-14 04:12:53 +0800

    delete unused  files

--- src/hooks/useUserContext.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import UserContext from "../contex/UserContext";
-
-export default function useUserContext() {
-  const { user, setUser } = useContext(UserContext);
-  return { user, setUser };
-}
